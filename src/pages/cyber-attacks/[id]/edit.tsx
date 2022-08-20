201b94f57dc49c09cf7a531c4d7edf80d85bc0e7
AmitMT <amit.mathov@gmail.com>
2022-08-20 10:46:07 +0300

    removed markdown edit and preview functionality

--- src/pages/cyber-attacks/[id]/edit.tsx
+++ src/pages/cyber-attacks/[id]/edit.tsx
@@ -1,65 +1,15 @@
-import React, { useState } from 'react';
+import React from 'react';
 
 import { NextPage } from 'next';
-import dynamic from 'next/dynamic';
 
-import '@uiw/react-markdown-editor/markdown-editor.css';
-import '@uiw/react-markdown-preview/markdown.css';
 import { setProtectedView } from '../../../lib/auth/serverSideSession';
-
-const MarkdownEditor = dynamic(() => import('@uiw/react-markdown-editor'), { ssr: false });
 
 export interface EditProps {}
 
-const Edit: NextPage<EditProps> = ({ ...props }) => {
-	const [value, setValue] = useState<string | undefined>(`# הקדמה
-# מושגים בסיסיים
-# החולשה
-# התקיפה
-# הגנה
-# הרחבה ומקורות נוספים
-[Learn
-Markdown](https://www.markdowntutorial.com)`);
-
-	const [editorTheme, setEditorTheme] = useState<'light' | 'dark'>('light');
-
+const Edit: NextPage<EditProps> = () => {
 	return (
-		<div
-			className="flex-1 flex flex-col items-center justify-center p-2"
-			dir="ltr"
-			{...props}
-			data-color-mode={editorTheme}
-		>
-			<button
-				className={`border-4 rounded-lg p-2 mb-2 font-semibold ${
-					editorTheme === 'light'
-						? 'bg-white text-black'
-						: 'bg-neutral-800 border-neutral-900 dark:border-none text-white'
-				}`}
-				onClick={() => setEditorTheme(editorTheme === 'light' ? 'dark' : 'light')}
-			>
-				Toggle Theme
-			</button>
-			<MarkdownEditor
-				value={value}
-				onChange={setValue}
-				className="flex-1 min-w-[56rem]"
-				visible
-				toolbars={[
-					'bold',
-					'italic',
-					'header',
-					'strike',
-					'underline',
-					'quote',
-					'olist',
-					'ulist',
-					'link',
-					'todo',
-					'image',
-				]}
-				toolbarsMode={['preview', 'fullscreen']}
-			/>
+		<div className="flex-1 flex flex-col items-center justify-center p-2">
+			<div>hi</div>
 		</div>
 	);
 };
