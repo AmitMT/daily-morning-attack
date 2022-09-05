4abf5939cf3e7bfd36bdce382ef0d24e03ea95d9
AmitMT <amit.mathov@gmail.com>
2022-09-05 20:33:31 +0300

    whyyy

--- src/pages/cyber-attacks/[id]/index.tsx
+++ src/pages/cyber-attacks/[id]/index.tsx
@@ -186,7 +186,7 @@ export const getServerSideProps = setServerSideSessionView<CyberAttackProps>(asy
 	} catch (e) {
 		return {
 			props: {
-				cyberAttack: JSON.parse(JSON.stringify(e.message)),
+				cyberAttack: JSON.parse(JSON.stringify((e as any).message)),
 			},
 		};
 	}
